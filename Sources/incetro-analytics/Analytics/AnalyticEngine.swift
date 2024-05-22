a3e9b311e0bd44bd77f702c7f0a34d0d79e4a97f
Andrey Barsukov <108578685+isNotAndy@users.noreply.github.com>
2024-05-22 17:09:31 +0300

    style: add mode property

--- Sources/incetro-analytics/Analytics/AnalyticEngine.swift
+++ Sources/incetro-analytics/Analytics/AnalyticEngine.swift
@@ -15,14 +15,18 @@ public final class AnalyticEngine {
     
     /// The analytics providers that the system can use to send events.
     private let providers: [any AnalyticProvider]
+    
+    /// The mode of operation for logging.
+    private let mode: DebugMode
 
     // MARK: - Initializers
     
     /// Initializes the analytics engine with the provided providers.
     ///
     /// - Parameter providers: The providers to be registered with the implementation.
-    public init(providers: [any AnalyticProvider]) {
+    public init(providers: [any AnalyticProvider], mode: DebugMode) {
         self.providers = providers
+        self.mode = mode
     }
 }
 
@@ -33,16 +37,25 @@ extension AnalyticEngine {
     /// Sends an analytics event to all configured analytics providers.
     ///
     /// - Parameter event: The analytics event to be sent.
-    public func send(event: any AnalyticsEvent) {
+    public func send(event: any AnalyticsEvent) throws {
         do {
             try providers.forEach { provider in
                 try provider.send(event: event)
-#if DEBUG
-print("send event (event.rawValue) for (provider.rawValue)")
-#endif
+                // Conditionally print debug message based on mode
+                if mode == .debug {
+                    print("send event \(event.rawValue) for \(provider.rawValue)")
+                    /// Errors: Value of type 'any AnalyticProvider' has no member 'rawValue'
+                    /// Value of type 'any AnalyticsEvent' has no member 'rawValue'
+                }
             }
-        } catch {
-            
         }
     }
+}
+
+// MARK: - DebugMode
+
+/// An enumeration representing the debug mode for logging.
+public enum DebugMode {
+    case debug
+    case normal
 }
