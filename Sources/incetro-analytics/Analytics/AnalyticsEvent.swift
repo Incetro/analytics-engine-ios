735e02d8558c303334a48c90216070dfb7d36e25
Andrey Barsukov <108578685+isNotAndy@users.noreply.github.com>
2024-05-22 14:24:50 +0300

    wip

--- Sources/incetro-analytics/Analytics/AnalyticsEvent.swift
+++ Sources/incetro-analytics/Analytics/AnalyticsEvent.swift
@@ -5,18 +5,3 @@
 //  Created by Andrey Barsukov on 16.05.2024.
 //
 
-import Foundation
-
-// MARK: - AnalyticsEvent
-
-public protocol AnalyticsEvent: Equatable {
-
-    /// The associated type for the metadata of the event.
-    associatedtype Metadata
-
-    /// The name of the analytics event.
-    var name: String { get }
-
-    /// The associated metadata for the event.
-    var metadata: Metadata { get }
-}
