0a157ee82b27ed1ebfea395741b884410ba7002c
Andrey Barsukov <108578685+isNotAndy@users.noreply.github.com>
2024-05-24 12:09:55 +0300

    chore: rename package

--- Package.swift
+++ Package.swift
@@ -4,20 +4,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "incetro-analytics",
+    name: "AnalyticsEngine",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "incetro-analytics",
-            targets: ["incetro-analytics"]),
+            name: "AnalyticsEngine",
+            targets: ["AnalyticsEngine"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "incetro-analytics"),
+            name: "AnalyticsEngine"),
         .testTarget(
-            name: "incetro-analyticsTests",
-            dependencies: ["incetro-analytics"]),
+            name: "AnalyticsEngine",
+            dependencies: ["AnalyticsEngine"]),
     ]
 )
